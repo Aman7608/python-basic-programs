9b9b6120fd8e03aaf8304060a1b14b4cc3173fec
Aman7608 <149958755+Aman7608@users.noreply.github.com>
2023-12-07 21:12:30 +0530

    Add files via upload

---
+++ my python files/basicprogramming/index.py
@@ -0,0 +1,36 @@
+# Variables
+name = "John"
+age = 25
+height = 5.9
+is_student = True
+
+# Print variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student?", is_student)
+
+# Arithmetic operations
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+# Lists
+fruits = ["apple", "banana", "orange"]
+print("Fruits:", fruits)
+
+# Accessing elements
+print("First fruit:", fruits[0])
+
+# Adding elements
+fruits.append("grape")
+print("Updated Fruits:", fruits)
